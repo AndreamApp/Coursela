84574d5ba13399bef727733221da7087ff91b7cf
Peng QiBiao <andreamapp@sina.com>
2019-03-22 19:59:08 +0800

    login activity layout

---
+++ app/src/main/java/app/andream/coursela/ui/LoginActivity.java
@@ -0,0 +1,21 @@
+package app.andream.coursela.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import app.andream.coursela.R;
+
+/**
+ * Created by Andream on 2019/3/22.
+ * Email: andreamapp@qq.com
+ * Website: http://andreamapp.com
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
