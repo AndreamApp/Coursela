fdd3e0d4af06097448f51db9eb199ef8ca2dc574
Peng QiBiao <andreamapp@sina.com>
2019-03-26 09:17:38 +0800

    Course Detail & Teacher Detail

--- app/src/main/java/app/andream/coursela/ui/CourseDetailActivity.java
+++ app/src/main/java/app/andream/coursela/ui/CourseDetailActivity.java
@@ -1,7 +1,9 @@
 package app.andream.coursela.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 import app.andream.coursela.R;
 import app.andream.coursela.bean.CourseDetail;
@@ -13,9 +15,28 @@ import app.andream.coursela.bean.CourseDetail;
  */
 public class CourseDetailActivity extends ResponseActivity<CourseDetail> {
 
+    TextView courseName, courseCode, academy, teacher, classroom, stucnt, schedule, credit, hours;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_detail);
+        initViews();
+    }
+
+    private void initViews() {
+        courseName = findViewById(R.id.tv_course_name);
+        courseCode = findViewById(R.id.tv_course_code);
+        academy = findViewById(R.id.tv_academy);
+        teacher = findViewById(R.id.tv_teacher);
+        classroom = findViewById(R.id.tv_classroom);
+        stucnt = findViewById(R.id.tv_course_stu_cnt);
+        schedule = findViewById(R.id.tv_schedule);
+        credit = findViewById(R.id.tv_credit);
+        hours = findViewById(R.id.tv_hours);
+        teacher.setOnClickListener(v -> {
+            // TODO: transfer teacher info
+            startActivity(new Intent(CourseDetailActivity.this, TeacherDetailActivity.class));
+        });
     }
 }
